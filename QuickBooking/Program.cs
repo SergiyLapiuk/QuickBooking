cb4b4faa7d24602e440fc7a3eb95488d39871c6f
SergiyLapiuk <sergiy.lapiuk@knu.ua>
2024-10-29 11:33:07 +0200

    swaggerfix

--- QuickBooking/Program.cs
+++ QuickBooking/Program.cs
@@ -15,12 +15,9 @@ builder.Services.AddDbContext<QuickBookingDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
